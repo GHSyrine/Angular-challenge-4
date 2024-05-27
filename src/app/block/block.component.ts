1436a3b3adbb828163700ef942b178078a234088
Syrine GHRIBI <ghribi.syrine.ghs@gmail.com>
2024-05-27 12:33:54 +0200

    add blockcomponent and update appcomponnent

---
+++ src/app/block/block.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css'
+})
+export class BlockComponent {
+}
